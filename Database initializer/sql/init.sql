d6df7de4d8f29a4ad0b3f65df7dcca84a47dcd68
TarabayIvan <ivantarabayit@gmail.com>
2024-05-27 21:57:46 -0300

    Inserts de prueba eliminados. Tarea #1

--- Database initializer/sql/init.sql
+++ Database initializer/sql/init.sql
@@ -24,10 +24,3 @@ CREATE TABLE IF NOT EXISTS responses (
     FOREIGN KEY (id_user) REFERENCES users(id_user),
     FOREIGN KEY (id_post) REFERENCES threads(id_post)
 );
-
-INSERT INTO users VALUES (NULL, 'usuario 1', 'contra123', 'respuesta uno', 'respuesta 2');
-INSERT INTO users VALUES (NULL, 'usuario 2', 'contra123', 'respuesta uno', 'respuesta 2');
-
-INSERT INTO threads VALUES (NULL, 1, 'comida', 'RECETA 1', 'texto post', '/image.png');
-
-INSERT INTO responses VALUES (NULL, 2, 1, 'respuesta a thread 1');
