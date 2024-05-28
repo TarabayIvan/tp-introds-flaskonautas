3b240a330e1bab26b853052c679e8b74911df342
FranciscoTapia <ftapia@fi.uba.ar>
2024-05-28 19:25:58 -0300

    Creacion de endpoint para mostrar posts. Tarea #10

--- API/app.py
+++ API/app.py
@@ -9,5 +9,27 @@ app = Flask(__name__)
 engine = create_engine("mysql+mysqlconnector://root:123@localhost/flaskonautas")
 
 
+@app.route('/get_posts', methods = ['GET'])
+def posts():
+    connection = engine.connect()
+    query = "SELECT * FROM posts;"
+    try:
+        data = connection.execute(text(query))
+        connection.close()
+    except SQLAlchemyError as err:
+        return jsonify(str(err.__cause__))
+    posts = []
+    for row in data:
+        entity = {}
+        entity['id_post'] = row.id_post
+        entity['id_user'] = row.id_user
+        entity['category'] = row.category
+        entity['title'] = row.title
+        entity['post'] = row.post
+        entity['image_link'] = row.image_link
+        posts.append(entity)
+    return jsonify(posts), 200
+
+
 if __name__ == "__main__":
-    app.run("127.0.0.1", port="3306")
+    app.run("127.0.0.1", port="5000")
