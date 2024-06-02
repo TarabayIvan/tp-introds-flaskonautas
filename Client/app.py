67e276a9c463f36026b70ed94a086a52067225f2
Jeremías Ferreyra Giorgi <165089541+fgiorgijeremias@users.noreply.github.com>
2024-06-01 22:59:51 -0300

    Creo la función para renderizar el template 404.html. Tarea #38

--- Client/app.py
+++ Client/app.py
@@ -53,6 +53,9 @@ def logout():
     session.pop('user', None)
     return redirect(url_for('login'))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run("127.0.0.1", port="5000")
