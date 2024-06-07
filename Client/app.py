a03a0bff5ba2649b4b04f1c899d77dc4e19f8d5b
guillsil <guasilva@fi.uba.ar>
2024-06-07 12:26:34 -0300

    #19, #18, #26 #59

--- Client/app.py
+++ Client/app.py
@@ -56,6 +56,22 @@ def login():
                 return render_template('login.html', error=error_message)
     return render_template('login.html')
 
+
+@app.route("/delete-account", methods=['GET', 'POST'])
+def delete_account():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user_credentials = {'username': username, 'password': password}
+        if username and password:
+            response = requests.post(API_URL + "/delete_user", json=user_credentials)
+            if response.status_code == 200:
+                session.pop('user', None)
+                return redirect(url_for('index'))
+            else:
+                error_message = "No se pudo eliminar la cuenta."
+                return render_template('user.html', error=error_message)
+    return render_template('delete-account.html')
 @app.route('/recovery', methods=['GET', 'POST'])
 def recovery():
     if request.method == "POST":
@@ -80,6 +96,29 @@ def recovery():
                 return render_template('recovery.html', error = error_msj)
     return render_template('recovery.html')
 
+@app.route('/modificar-password', methods=['GET', 'POST'])
+def modificar_password():
+    if request.method == "POST":
+        username = request.form.get('user')
+        new_password = request.form.get('newpword')
+        security_answer_1 = request.form.get('secquestions1')
+        security_answer_2 = request.form.get('secquestions2')
+        new_password_hashed = generate_password_hash(new_password)
+        user_credentials = {'username': username, 'password': new_password_hashed, 'security_answer_one': security_answer_1 , 'security_answer_two': security_answer_2 }
+        if username and new_password and security_answer_1 and security_answer_2:
+            respone = requests.patch(API_URL + '/update_password', json = user_credentials)
+            if respone.status_code == 200:
+                return redirect(url_for('login')) # Redirige a login si se cambio la contraseña
+            elif respone.status_code == 401:
+                error_msj = "Las respuestas a las preguntas de seguridad son incorrectas."
+                return render_template('refactor_password.html', error = error_msj)
+            elif respone.status_code == 404:
+                error_msj = "El usuario no existe."
+                return render_template('refactor_password.html', error = error_msj)
+            else:
+                error_msj = "Hubo un problema al cambiar la contraseña"
+                return render_template('refactor_password.html', error = error_msj)
+    return render_template('refactor_password.html')
 @app.route("/logout")
 def logout():
     session.pop('user', None)
@@ -92,11 +131,11 @@ def get_user_data():
         response = requests.get(API_URL + f"/user/{user_id}")
         if response.status_code == 200:
             user_data = response.json()
-            return jsonify(user_data)
+            return render_template("user.html", user_data=user_data)
         else:
-            return jsonify({"error": "No se pudieron obtener los datos del usuario"}), 400
+            return render_template("user.html", user_data={"username": "Usuario no autenticado", "password": "Usuario no autenticado"})
     else:
-        return jsonify({"error": "Usuario no autenticado"}), 401
+        return render_template("user.html", user_data={"username": "Usuario no autenticado", "password": "Usuario no autenticado"})
 
 @app.route("/categories", methods=['GET'])
 def categories():
--- Client/static/scripts/script.js
+++ Client/static/scripts/script.js
@@ -1,14 +1,25 @@
-const navLinks = document.getElementById("navLinks");
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.getElementById("navLinks");
 
-const showMenu = () => {
-    navLinks.style.top = '0';
-};
+    document.getElementById('toggle-password').addEventListener('click', function() {
+        var passwordElement = document.getElementById('password');
+        if (passwordElement.classList.contains('hidden')) {
+            passwordElement.classList.remove('hidden');
+        } else {
+            passwordElement.classList.add('hidden');
+        }
+    });
 
-const hideMenu = () => {
-    navLinks.style.top = '-800px';
-};
+    const showMenu = () => {
+        navLinks.style.top = '0';
+    };
 
-document.getElementById('user-logo').addEventListener('click', (event) => {
-    event.preventDefault();
-    window.location.href = './user';
+    const hideMenu = () => {
+        navLinks.style.top = '-800px';
+    };
+
+    document.getElementById('user-logo').addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.href = './user';
+    });
 });
