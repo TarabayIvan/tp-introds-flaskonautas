bb4b2291b3b13d724e5cfb3d942af64ec8c756ca
guillsil <guasilva@fi.uba.ar>
2024-06-12 01:05:41 -0300

    Solucionado bug con el archivo de java script, ahora se asegura que exista cada elemento con el id correspondiente antes de realizare alguna operacion

--- Client/static/scripts/script.js
+++ Client/static/scripts/script.js
@@ -1,51 +1,61 @@
-const navLinks = document.getElementById("navLinks");
-window.showMenu = function() {
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.getElementById("navLinks");
+    const togglePassword = document.getElementById('toggle-password');
+    const userLogo = document.getElementById('user-logo');
+    const searchInput = document.getElementById('searchInput');
+    const postImage = document.getElementById('post-image');
+
+    window.showMenu = function() {
         navLinks.style.top = '0';
     };
 
-window.hideMenu = function() {
-    navLinks.style.top = '-800px';
-};
-document.addEventListener('DOMContentLoaded', function() {
-    const togglePassword = document.getElementById('toggle-password');
+    window.hideMenu = function() {
+        navLinks.style.top = '-800px';
+    };
 
-    togglePassword.addEventListener('click', function() {
-        var passwordElement = document.getElementById('password');
-        if (passwordElement.classList.contains('hidden')) {
-            passwordElement.classList.remove('hidden');
-        } else {
-            passwordElement.classList.add('hidden');
-        }
-    });
+    if (togglePassword) {
+        togglePassword.addEventListener('click', function() {
+            var passwordElement = document.getElementById('password');
+            if (passwordElement.classList.contains('hidden')) {
+                passwordElement.classList.remove('hidden');
+            } else {
+                passwordElement.classList.add('hidden');
+            }
+        });
+    }
 
-    document.getElementById('user-logo').addEventListener('click', (event) => {
-        event.preventDefault();
-        window.location.href = './user';
-    });
-});
+    if (userLogo) {
+        userLogo.addEventListener('click', (event) => {
+            event.preventDefault();
+            window.location.href = './user';
+        });
+    }
 
+    if (searchInput) {
+        searchInput.addEventListener('input', function(e) {
+            let searchValue = e.target.value.toLowerCase();
+            let posts = document.getElementsByClassName('card');
 
-document.getElementById('searchInput').addEventListener('input', function(e) {
-    let searchValue = e.target.value.toLowerCase();
-    let posts = document.getElementsByClassName('card');
+            for (let i = 0; i < posts.length; i++) {
+                let post = posts[i];
+                let postTitle = post.getAttribute('data-title');
 
-    for (let i = 0; i < posts.length; i++) {
-        let post = posts[i];
-        let postTitle = post.getAttribute('data-title');
+                if (postTitle.indexOf(searchValue) > -1) {
+                    post.style.display = '';
+                } else {
+                    post.style.display = 'none';
+                }
+            }
+        });
+    }
 
-        if (postTitle.indexOf(searchValue) > -1) {
-            post.style.display = '';
-        } else {
-            post.style.display = 'none';
-        }
+    if (postImage) {
+        postImage.addEventListener('change', function () {
+            let size = this.files[0].size;
+            if (size > 2 * 1024 * 1024) { // Limita el peso de la imagen a 2mb
+                alert('El archivo es demasiado grande, el tamaño máximo permitido es de 2MB');
+                this.value = '';
+            };
+        });
     }
 });
-
-
-document.getElementById('post-image').addEventListener('change', function () {
-    let size = this.files[0].size;
-    if (size > 2 * 1024 * 1024) { // Limita el peso de la imagen a 2mb
-      alert('El archivo es demasiado grande, el tamaño máximo permitido es de 2MB');
-      this.value = '';
-    };
-  });
